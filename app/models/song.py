c1a878e3cea42059676a53bfe485096e8e7dadaa
GalaxyPhoenix <galaxyphoenix716@gmail.com>
2026-05-28 13:44:23 +0530

    added song model

---
+++ app/models/song.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from sqlmodel import TEXT, VARCHAR, Column
+
+
+class Song(BaseModel):
+    __tablename__ = "songs"
+
+    id = Column(TEXT, primary_key=True)
+    song_url = Column(TEXT)
+    thumbnail_url = Column(TEXT)
+    artist = Column(TEXT)
+    song_name = Column(VARCHAR(100))
+    hex_code = Column(VARCHAR(6))
